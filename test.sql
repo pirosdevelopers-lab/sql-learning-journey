3f9f34462fd7272d830ab793a221e391ae875e64
Piros <piros.developers@gmail.com>
2025-10-05 18:43:22 +0700

    practice: complete SQL day 1 - SELECT basics

---
+++ test.sql
@@ -0,0 +1,14 @@
+-- Day 1: SQL SELECT Basics
+-- Date: 2025-10-05
+-- Select all columns
+SELECT *
+FROM employees;
+-- Select specific columns
+SELECT first_name,
+    last_name,
+    salary
+FROM employees;
+-- What I learned today:
+-- - Basic SELECT syntax
+-- - Selecting all vs specific columns
+-- - SQL is not case-sensitive but UPPERCASE for keywords is convention
